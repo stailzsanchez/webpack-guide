de9ae4b22529c3ff7122a79deefeb091f1cacd8a
stailzsanchez <stailzsanchez@gmail.com>
2026-05-23 23:07:12 +0300

    decompose webpack config

---
+++ src/config/build/buildPlugins.ts
@@ -0,0 +1,30 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack, { Configuration, DefinePlugin } from "webpack";
+import { BuildOptions } from "./types/types";
+
+export function buildPlugins(options: BuildOptions): Configuration["plugins"] {
+  const { mode, paths } = options;
+
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+
+  const plugins: Configuration["plugins"] = [
+    new HtmlWebpackPlugin({ template: paths.html }),
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      }),
+    );
+  }
+
+  return plugins;
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,66 +1,25 @@
 import path from "node:path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-
-type Mode = "production" | "development";
+import { buildWebpack } from "./src/config/build/buildWebpack";
+import { BuildMode, BuildPaths } from "./src/config/build/types/types";
 
 interface EnvVariables {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
 }
 
 export default (env: EnvVariables) => {
-  const isDev = env.mode === "development";
-  const isProd = env.mode === "production";
-
-  const devServer = {
-    port: env.port ?? 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  };
-
-  const config = {
-    mode: env.mode || "development",
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, "build"),
     entry: path.resolve(__dirname, "src", "index.tsx"),
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
-          ],
-        },
-        { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
-      ],
-    },
-
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }),
-      isDev && new webpack.ProgressPlugin(),
-      isProd &&
-        new MiniCssExtractPlugin({
-          filename: "css/[name].[contenthash:8].css",
-          chunkFilename: "css/[name].[contenthash:8].css",
-        }),
-    ].filter(Boolean),
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devtool: isDev && "inline-source-map",
-    devServer: isDev ? devServer : undefined,
+    html: path.resolve(__dirname, "public", "index.html"),
   };
+
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths: paths,
+  });
 
   return config;
 };
