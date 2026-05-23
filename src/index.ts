8266137268e7874bccb311b0282fd0ae2a9f1a70
stailzsanchez <stailzsanchez@gmail.com>
2026-05-23 18:55:12 +0300

    ts install

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { calc } from "./test";
+
+console.log("Hello");
+console.log(calc(1, 2));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+  };
+};
